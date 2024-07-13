ae6599816a7baeee5b737589cd701888aafae911
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-13 12:38:44 -0500

    Completing the processArguments function.

--- src/arguments.js
+++ src/arguments.js
@@ -3,15 +3,18 @@
  * the given argument presents, eventually becoming interactive to prompt
  * for the missing / invalid data.
  * @param hre The hardhat runtime environment.
- * @param argumentsSpec The arguments spec.
+ * @param arguments The arguments spec.
  * @param givenArguments The actual arguments.
  * @param nonInteractive Whether to raise an error when this method is about
  * to become interactive.
  * @returns {Promise<*[]>} The processed arguments as an array (async function).
  */
-async function processArguments(hre, argumentsSpec, givenArguments, nonInteractive) {
-    // TODO
-    return [];
+async function processArguments(hre, arguments, givenArguments, nonInteractive) {
+    const prompts = hre.blueprints.prepareArgumentPrompts(arguments, nonInteractive, givenArguments);
+    const results = await new hre.enquirerPlus.Enquirer().prompt(prompts);
+    return arguments.map(({name}) => {
+        return results[name];
+    });
 }
 
 module.exports = {
