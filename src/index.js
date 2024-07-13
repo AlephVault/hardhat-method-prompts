969c0212a981057b00ef7573f0c546183e2c6016
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-13 15:42:11 -0500

    Starting the ContractMethodCall class.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+const {invoke} = require("./method-call");
+
+class ContractMethodCall {
+    construct(methodType, name, {onError, onSuccess}, argumentsSpec, txOptionsSpec) {
+        this._method = {type: methodType, name, onError, onSuccess};
+        this._argumentsSpec = argumentsSpec || [];
+        this._txOptionsSpec = txOptionsSpec || {};
+    }
+
+    async invokeOnContract(
+        hre, contract, givenArguments, givenTxOptions, nonInteractive
+    ) {
+        await invoke(
+            hre, contract, this._method, this._argumentsSpec, givenArguments,
+            this._txOptionsSpec, givenTxOptions, nonInteractive
+        );
+    }
+
+    async invokeOnDeployedContract(
+        hre, deploymentId, deploymentContractId, givenArguments, givenTxOptions, nonInteractive
+    ) {
+        const contract = await hre.common.getDeployedContract(deploymentContractId, deploymentId);
+        return await this.invokeOnContract(
+            hre, contract, givenArguments, givenTxOptions, nonInteractive
+        );
+    }
+}
+
 module.exports = {};
