761bbbb8531f19bd6339bfa307aac925339f8814
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-14 09:30:44 -0500

    Adding the sample project.

---
+++ sample-project/hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-common-tools");
+require("hardhat-enquirer-plus");
+require("hardhat-blueprints");
+require("..");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24"
+};
