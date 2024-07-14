163a45ff112aa856c95ce32ca2eaa6ddfeab9a13
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-14 10:24:23 -0500

    Adding the intermediate class to automatically populate the hre instance for the calls, and adding the default module and contract to play with.

--- sample-project/hardhat.config.js
+++ sample-project/hardhat.config.js
@@ -2,7 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-common-tools");
 require("hardhat-enquirer-plus");
 require("hardhat-blueprints");
+require("hardhat-openzeppelin-common-blueprints");
 require("..");
+const {task} = require("hardhat/config");
+
+task("sample-mint", "Invokes")
+    .setAction((args, hre, runSuper) => {
+
+    });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,37 +1,55 @@
 const {invoke} = require("./method-call");
 const {extendEnvironment} = require("hardhat/config");
 
-class ContractMethodPrompt {
-    construct(methodType, name, {onError, onSuccess}, argumentsSpec, txOptionsSpec) {
+class _ContractMethodPrompt {
+    constructor(hre, methodType, name, {onError, onSuccess}, argumentsSpec, txOptionsSpec) {
+        this._hre = hre;
         this._method = {type: methodType, name, onError, onSuccess};
         this._argumentsSpec = argumentsSpec || [];
         this._txOptionsSpec = txOptionsSpec || {};
     }
 
     async invokeOnContract(
-        hre, contract, givenArguments, givenTxOptions, nonInteractive
+        contract, givenArguments, givenTxOptions, nonInteractive
     ) {
+        if (!contract)
         await invoke(
-            hre, contract, this._method, this._argumentsSpec, givenArguments,
-            this._txOptionsSpec, givenTxOptions, nonInteractive
+            this._hre, contract, this._method, this._argumentsSpec, givenArguments || [],
+            this._txOptionsSpec, givenTxOptions || {}, nonInteractive || false
         );
     }
 
     async invokeOnDeployedContract(
-        hre, deploymentId, deploymentContractId, givenArguments, givenTxOptions, nonInteractive
+        deploymentId, deploymentContractId, givenArguments, givenTxOptions, nonInteractive
     ) {
-        const contract = await hre.common.getDeployedContract(deploymentContractId, deploymentId);
+        // 1. Get the contract.
+        let contract = null;
+        try {
+            contract = await this._hre.common.getDeployedContract(deploymentContractId, deploymentId);
+        } catch(e) {}
+        if (!contract) {
+            console.log(`The ${deploymentContractId} seems to be not deployed in the current deployment`);
+            return;
+        }
+
+        // 2. Use the contract.
         console.log(
             `Using deployed contract: ${deploymentContractId}` +
             (deploymentId ? ` (deployment id: ${deploymentId})` : "")
         );
         return await this.invokeOnContract(
-            hre, contract, givenArguments, givenTxOptions, nonInteractive
+            contract, givenArguments, givenTxOptions, nonInteractive
         );
     }
 }
 
 extendEnvironment((hre) => {
+    class ContractMethodPrompt extends _ContractMethodPrompt {
+        constructor(...args) {
+            super(hre, ...args);
+        }
+    }
+
     hre.methodPrompts = {ContractMethodPrompt}
 })
 
