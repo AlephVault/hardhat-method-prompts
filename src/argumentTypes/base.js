082605c4566e41da96259cd88620d19db7c7d79e
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-16 10:06:03 -0500

    Adding the base blueprint prompt, intended for arrays and tuples.

---
+++ src/argumentTypes/base.js
@@ -0,0 +1,32 @@
+const {Prompt, Input} = require("enquirer");
+
+class BaseBlueprintPrompt extends Prompt {
+    constructor({hre, nonInteractive, given, ...options}) {
+        super(options);
+        this._hre = hre;
+        this._given = given;
+        this._nonInteractive = nonInteractive
+    }
+
+    /**
+     * Prompts some blueprint arguments with their given values.
+     * @param blueprintArgs The arguments to prepare and prompt.
+     * @param given_ The given values.
+     * @returns {Promise<*>} The result (async function).
+     */
+    async _apply(blueprintArgs, given_) {
+        return await new this._hre.enquirerPlus.Enquirer().prompt(this._hre.blueprints.prepareArgumentPrompts(
+            blueprintArgs, this._nonInteractive, given_
+        ));
+    }
+
+    async render() {
+        // This one does not need any implementation on its own.
+    }
+
+    async run() {
+        throw new Error("This prompt must be implemented in terms of _apply() calls");
+    }
+}
+
+module.exports = {BaseBlueprintPrompt};
