1f511171971249e5f1fc2273760db49c8d82ac85
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-23 18:28:13 -0500

    Moving the native tasks to the native.js file.

--- sample-project/hardhat.config.js
+++ sample-project/hardhat.config.js
@@ -64,48 +64,6 @@ extendEnvironment((hre) => {
             argumentType: "uint256"
         }], {}
     ).asTask("sample-balance-of", "Invokes an ERC1155 balanceOf", {onlyExplicitTxOptions: true});
-
-    new hre.methodPrompts.CustomPrompt(
-        function([address]) {
-            return hre.common.getBalance(address);
-        }, {
-            onError: (e) => {
-                console.error("There was an error while getting the balance");
-                console.error(e);
-            },
-            onSuccess: (tx) => {
-                console.log("The method ran successfully:", tx);
-            }
-        }, [{
-            name: "address",
-            description: "The address (or account index) to query the balance for",
-            message: "What's the address (or account index) you want to query the balance for?",
-            argumentType: "smart-address"
-        }], {}
-    ).asTask("balance-of", "Gets the native balance of for an account", {onlyExplicitTxOptions: true});
-
-    new hre.methodPrompts.CustomPrompt(
-        function([address], txOpts) {
-            return hre.common.transfer(address, txOpts);
-        }, {
-            onError: (e) => {
-                console.error("There was an error while getting the balance");
-                console.error(e);
-            },
-            onSuccess: (tx) => {
-                console.log("The method ran successfully:", tx);
-            }
-        }, [{
-            name: "address",
-            description: "The address (or account index) to send native tokens to",
-            message: "What's the address (or account index) to send native tokens to?",
-            argumentType: "smart-address"
-        }], {
-            value: {onAbsent: "prompt"},
-            account: {onAbsent: "default"},
-            gasPrice: {onAbsent: "default"}
-        }
-    ).asTask("transfer", "Transfers native balance to another account");
 });
 
 /** @type import('hardhat/config').HardhatUserConfig */
--- src/tasks/native.js
+++ src/tasks/native.js
@@ -1,2 +1,45 @@
-// Native/custom: balanceOf(address)
-// Native/custom: transfer(address, amount)
+const {extendEnvironment} = require("hardhat/config");
+
+extendEnvironment((hre) => {
+    new hre.methodPrompts.CustomPrompt(
+        function([address]) {
+            return hre.common.getBalance(address);
+        }, {
+            onError: (e) => {
+                console.error("There was an error while getting the balance");
+                console.error(e);
+            },
+            onSuccess: (tx) => {
+                console.log("The method ran successfully:", tx);
+            }
+        }, [{
+            name: "address",
+            description: "The address (or account index) to query the balance for",
+            message: "What's the address (or account index) you want to query the balance for?",
+            argumentType: "smart-address"
+        }], {}
+    ).asTask("balance-of", "Gets the native balance of for an account", {onlyExplicitTxOptions: true});
+
+    new hre.methodPrompts.CustomPrompt(
+        function([address], txOpts) {
+            return hre.common.transfer(address, txOpts);
+        }, {
+            onError: (e) => {
+                console.error("There was an error while getting the balance");
+                console.error(e);
+            },
+            onSuccess: (tx) => {
+                console.log("The method ran successfully:", tx);
+            }
+        }, [{
+            name: "address",
+            description: "The address (or account index) to send native tokens to",
+            message: "What's the address (or account index) to send native tokens to?",
+            argumentType: "smart-address"
+        }], {
+            value: {onAbsent: "prompt"},
+            account: {onAbsent: "default"},
+            gasPrice: {onAbsent: "default"}
+        }
+    ).asTask("transfer", "Transfers native balance to another account");
+});
