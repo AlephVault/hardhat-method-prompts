13a24ab19d585d8cead9377a73e340235f058f92
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-23 18:39:36 -0500

    Adding the totalSupply() method to ERC-20 file.

--- src/tasks/erc20.js
+++ src/tasks/erc20.js
@@ -4,3 +4,18 @@
 // function allowance(address owner, address spender) external view returns (uint256);
 // function approve(address spender, uint256 value) external returns (bool);
 // function transferFrom(address from, address to, uint256 value) external returns (bool);
+const {extendEnvironment} = require("hardhat/config");
+
+extendEnvironment((hre) => {
+    new hre.methodPrompts.ContractMethodPrompt(
+        "call", "totalSupply", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: (value) => {
+                console.log("Total Supply:", value);
+            }
+        }, [], {}
+    ).asTask("erc20:total-supply", "Invokes totalSupply()(uint256) on an ERC-20 contract", {onlyExplicitTxOptions: true});
+});
