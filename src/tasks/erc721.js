d5a4b174b1e3da01977e4a1b499d088c6ab66acd
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-23 20:18:48 -0500

    Adding the safe-transfer-from methods.

--- src/tasks/erc721.js
+++ src/tasks/erc721.js
@@ -46,5 +46,62 @@ extendEnvironment((hre) => {
             message: "Who do you want to query the balance for?",
             argumentType: "smart-address"
         }], {}
-    ).asTask("erc721:owner-of", "Invokes ownerOf(address)(uint256) on an ERC-721 contract");
+    ).asTask("erc721:owner-of", "Invokes ownerOf(uint256)(address) on an ERC-721 contract");
+    new hre.methodPrompts.ContractMethodPrompt(
+        "send", "safeTransferFrom(address,address,uint256)", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: (tx) => {
+                console.log("Token transferred successfully. Transaction is:", tx);
+            }
+        }, [{
+            name: "from",
+            description: "The address to send the token from",
+            message: "Who do you want to send the token from? (must be you or must approve you)",
+            argumentType: "smart-address"
+        }, {
+            name: "to",
+            description: "The address to send the token to",
+            message: "Who do you want to send the token to?",
+            argumentType: "smart-address"
+        }, {
+            name: "tokenId",
+            description: "The ID of the token to send",
+            message: "What's the ID of the token to send?",
+            argumentType: "uint256"
+        }], {}
+    ).asTask("erc721:safe-transfer-from", "Invokes safeTransferFrom(address,address,uint256) on an ERC-721 contract");
+    new hre.methodPrompts.ContractMethodPrompt(
+        "send", "safeTransferFrom(address,address,uint256,bytes)", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: (tx) => {
+                console.log("Token transferred successfully. Transaction is:", tx);
+            }
+        }, [{
+            name: "from",
+            description: "The address to send the token from",
+            message: "Who do you want to send the token from? (must be you or must approve you)",
+            argumentType: "smart-address"
+        }, {
+            name: "to",
+            description: "The address to send the token to",
+            message: "Who do you want to send the token to?",
+            argumentType: "smart-address"
+        }, {
+            name: "tokenId",
+            description: "The ID of the token to send",
+            message: "What's the ID of the token to send?",
+            argumentType: "uint256"
+        }, {
+            name: "data",
+            description: "The data for this operation",
+            message: "Data for this send (use direct 0x for no data, or a dumb empty text input)",
+            argumentType: "hashed"
+        }], {}
+    ).asTask("erc721:safe-transfer-from-with-data", "Invokes safeTransferFrom(address,address,uint256,bytes) on an ERC-721 contract");
 });
