22141766482bb188f5ad0919ca81a3ebf4e5f07d
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-23 19:33:54 -0500

    Adding the allowance method task.

--- src/tasks/erc20.js
+++ src/tasks/erc20.js
@@ -1,5 +1,3 @@
-// function transfer(address to, uint256 value) external returns (bool);
-// function allowance(address owner, address spender) external view returns (uint256);
 // function approve(address spender, uint256 value) external returns (bool);
 // function transferFrom(address from, address to, uint256 value) external returns (bool);
 const {extendEnvironment} = require("hardhat/config");
@@ -53,4 +51,25 @@ extendEnvironment((hre) => {
             argumentType: "uint256"
         }], {}
     ).asTask("erc20:transfer", "Invokes transfer(address,uint256) on an ERC-20 contract");
+    new hre.methodPrompts.ContractMethodPrompt(
+        "call", "allowance", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: (value) => {
+                console.log("Allowed:", value);
+            }
+        }, [{
+            name: "owner",
+            description: "The owner of the tokens",
+            message: "Who'll be the owner you want to query for?",
+            argumentType: "smart-address"
+        }, {
+            name: "allowed",
+            description: "The user who ask the allowance for",
+            message: "Who'll be the user to query the # of allowed tokens?",
+            argumentType: "smart-address"
+        }], {}
+    ).asTask("erc20:allowance", "Invokes allowance(address,address)(uint256) on an ERC-20 contract", {onlyExplicitTxOptions: true});
 });
