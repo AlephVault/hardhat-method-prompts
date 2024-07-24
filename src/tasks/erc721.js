b03fa85b573fd2a5e5375c0628e22a53c6ec227f
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-23 20:02:23 -0500

    Adding ERC-721 methods: ownerOf and balanceOf.

--- src/tasks/erc721.js
+++ src/tasks/erc721.js
@@ -1,5 +1,3 @@
-// function balanceOf(address owner) external view returns (uint256 balance);
-// function ownerOf(uint256 tokenId) external view returns (address owner);
 // function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
 // function safeTransferFrom(address from, address to, uint256 tokenId) external;
 // function transferFrom(address from, address to, uint256 tokenId) external;
@@ -7,3 +5,46 @@
 // function setApprovalForAll(address operator, bool approved) external;
 // function getApproved(uint256 tokenId) external view returns (address operator);
 // function isApprovedForAll(address owner, address operator) external view returns (bool);
+const {extendEnvironment} = require("hardhat/config");
+
+extendEnvironment((hre) => {
+    new hre.methodPrompts.ContractMethodPrompt(
+        "call", "balanceOf", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: (value) => {
+                console.log("Balance:", value);
+            }
+        }, [{
+            name: "address",
+            description: "The address to query the balance for",
+            message: "Who do you want to query the balance for?",
+            argumentType: "smart-address"
+        }], {}
+    ).asTask("erc721:balance-of", "Invokes balanceOf(address)(uint256) on an ERC-721 contract");
+    new hre.methodPrompts.ContractMethodPrompt(
+        "call", "ownerOf", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: async (value) => {
+                console.log("Owner:", value);
+                const signers = await hre.common.getSigners();
+                for(let index = 0; index < signers.length; index++) {
+                    let address = hre.common.getAddress(signers[index]);
+                    if (address.toLowerCase() === value.toLowerCase()) {
+                        console.log("This address belongs to the account with index:", index);
+                    }
+                }
+            }
+        }, [{
+            name: "address",
+            description: "The address to query the balance for",
+            message: "Who do you want to query the balance for?",
+            argumentType: "smart-address"
+        }], {}
+    ).asTask("erc721:owner-of", "Invokes ownerOf(address)(uint256) on an ERC-721 contract");
+});
