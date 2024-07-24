43352961a7166faa87028c6a6ed29c4a12b2a2b8
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-23 22:08:03 -0500

    Adding the ERC-1155 balanceOf function.

--- src/tasks/erc1155.js
+++ src/tasks/erc1155.js
@@ -1,4 +1,3 @@
-// function balanceOf(address account, uint256 id) external view returns (uint256);
 // function balanceOfBatch(
 //     address[] calldata accounts,
 //     uint256[] calldata ids
@@ -13,3 +12,28 @@
 //     uint256[] calldata values,
 //     bytes calldata data
 // ) external;
+const {extendEnvironment} = require("hardhat/config");
+
+extendEnvironment((hre) => {
+    new hre.methodPrompts.ContractMethodPrompt(
+        "call", "balanceOf", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: (value) => {
+                console.log("Balance:", value);
+            }
+        }, [{
+            name: "address",
+            description: "The address to query the balance for",
+            message: "Who do you want to query the balance for?",
+            argumentType: "smart-address"
+        }, {
+            name: "uint256",
+            description: "The ID of the token to query the balance for",
+            message: "What's the ID of the token you want to query the balance for?",
+            argumentType: "uint256"
+        }], {}
+    ).asTask("erc1155:balance-of", "Invokes balanceOf(address,uint256) on an ERC-1155 contract");
+});
